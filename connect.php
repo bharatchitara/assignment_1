07b985aa504fb6d419341b22066edce5b092e476
bharat chitara <bharatchitara99@gmail.com>
2019-08-16 14:39:39 +0530

    search button added.

--- connect.php
+++ connect.php
@@ -1,12 +1,10 @@
 <?php
-//include 'search.php';
                                             
 $name = filter_input(INPUT_POST,"name");                                        // to create the variables.
 $email=filter_input(INPUT_POST,"email",FILTER_VALIDATE_EMAIL);
 $number = filter_input(INPUT_POST,"phone");
 
 $link = mysqli_connect("127.0.0.1", "root", "123456", "you");                    //to create connection. 
-mysqli_select_db($link,"you");
 
 if (!$link) {
 echo "some error occurs.";	
@@ -22,27 +20,17 @@ else
 {
     echo "please fill the details again.1" ;
 }
-
-
-$search = filter_input(INPUT_POST,"search");
-
-
-$sqll= "SELECT * FROM test where email like '%$search%' or phone like'%$search%'";
-$count = mysqli_num_rows($sqll);
-var t= mysqli_num_row(sqll);
-echo "t";
-
-if($count==0){
-	$output='No result found.';
+$set=$_POST['search'];
+if($set){
+$show ="SELECT * FROM user where Email='$set'";
+$result = mysqli_query($connect, $show);
+while ($rows=mysqli_fetch_array($result))
+ {
+echo $rows['Name'];
+echo $rows['Contact'];
+  }
 }
-else {
-	while($row= mysqli_fetch_array($sqll)){
-		$name = $row['name'];
-		$email= $row['email'];
-		$number= $row['phone'];
-	}
+else{
+echo "noTHING FOUND";
 }
-$output = '<div>'.$name.' '.$email.'</div>';
-print "$output";
-
 ?>
