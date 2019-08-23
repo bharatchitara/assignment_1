a44edb082e3c83f89ada4c6f98195eef45b0740c
bharat chitara <bharatchitara99@gmail.com>
2019-08-23 14:19:45 +0530

    email & phone validations added.

---
+++ search.php
@@ -0,0 +1,30 @@
+
+<?php
+
+$link = mysqli_connect("127.0.0.1", "root", "123456", "you"); 
+
+$search=$_POST['search'];
+
+if($search){
+	
+$show ="SELECT * FROM test where email='$search'";
+$result = mysqli_query($link, $show);
+
+while ($rows=mysqli_fetch_array($result))
+ {
+	 
+	 
+echo "Name of User : ";
+echo $rows['name'];
+echo "<br>";
+echo "Email ID : ";
+echo $rows['email'];
+echo "<br>";
+echo "Contact Number : ";
+echo $rows['phone'];
+exit;
+  }
+
+echo "NOTHING FOUND";
+}
+?>
